494a90d58a44f874f429fdc6dc3fa110581dba3f
shuto94 <kbmc1403@gmail.com>
2021-04-04 23:35:43 +0900

    修正2完了

--- index.php
+++ index.php
@@ -25,13 +25,12 @@ $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <h2>本日のご紹介ペット</h2>
     <ul>
         <?php foreach ($animals as $animal): ?>
-            <li><?= h($animal['type']) . 'の' .
+        <li><?= h($animal['type']) . 'の' .
             h($animal['classification']) .'ちゃん' . '<br>' . 
-            h($animal['description']) . '<br>' . h($animal['birthday']) . ' 生まれ' . '<br>' . 
-            '出身地 ' . h($animal['birthplace']) . 
-            '<hr>' 
-        ?>
-    </li>
+            h($animal['description']) . '<br>' .
+            h($animal['birthday']) . ' 生まれ' . '<br>' . '出身地 ' . 
+            h($animal['birthplace']) . '<hr>' ?>
+        </li>
         <?php endforeach; ?>
     </ul>
 </body>
