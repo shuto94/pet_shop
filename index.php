c08af4de6bc8772e26d900ac6929588035930d15
shuto94 <kbmc1403@gmail.com>
2021-04-04 16:56:56 +0900

    修正完了

--- index.php
+++ index.php
@@ -19,12 +19,19 @@ $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pet_shop - index</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h2>本日のご紹介ペット</h2>
     <ul>
         <?php foreach ($animals as $animal): ?>
-            <li style="list-style-type:none;"><?= h($animal['type']) . 'の' , ($animal['classification']) .'ちゃん' . '<br>' . ($animal['description']) . '<br>' . ($animal['birthday']) . ' 生まれ' . '<br>' . '出身地 ' . ($animal['birthplace']) . '<hr>' ?></li>
+            <li><?= h($animal['type']) . 'の' .
+            h($animal['classification']) .'ちゃん' . '<br>' . 
+            h($animal['description']) . '<br>' . h($animal['birthday']) . ' 生まれ' . '<br>' . 
+            '出身地 ' . h($animal['birthplace']) . 
+            '<hr>' 
+        ?>
+    </li>
         <?php endforeach; ?>
     </ul>
 </body>
